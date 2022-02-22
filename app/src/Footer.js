22633f1a6c51f01b882f604c07baa41aeeeafe7c
Horacio Gutierrez <horacio.estevez@gmail.com>
2022-02-21 23:02:14 -0300

    Archivos de clase 2 y 3

---
+++ app/src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            soy footer
+        </footer>
+    )
+}
+
+export default Footer
---
+++ app/src/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header>
+            <h1>Titulo</h1>
+            <nav>
+                <a href="#">links</a>
+                <a href="#">links</a>
+                <a href="#">links</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
