bdadebf104cc265040d410d1b65586989d5e83fc
Horacio Gutierrez <horacio.estevez@gmail.com>
2022-02-22 22:06:32 -0300

    resuelvo desafio clase N

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,17 @@
 import Header from "./Header"
 import Footer from "./Footer"
+//import container from react-bootstrap
+import { Container } from 'react-bootstrap'
 
 function App() {
     return (
         <>
             <Header />
+            <Container as="main">
+            {/* <main className="container"> */}
+                <div></div>
+            {/* </main> */}
+            </Container>
             <Footer/>
         </>
     )
