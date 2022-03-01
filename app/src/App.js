49565e44aa2866f984639b0a233eda522fe2486a
Horacio Gutierrez <horacio.gutierrez@educacionit.com.ar>
2022-02-28 22:45:02 -0300

    Componentes 2

--- app/src/App.js
+++ app/src/App.js
@@ -6,11 +6,12 @@ function App() {
 
     const edad = 32
     const usuarios = ["Juan", "Pedro", "Maria"]
+    const miOnAdd = () => {}
 
     return (
         <>
             <Header/>
-            <Main nombre="Horacio" apellido="Gutierrez" edad={edad} usuarios={usuarios}>
+            <Main nombre="Horacio" apellido="Gutierrez" onAdd={miOnAdd} edad={edad} usuarios={usuarios} initial={1}>
                 <p>Hola Mundo</p>
                 <p>Hola Mundo Dos</p>
             </Main>
--- app/src/Main.js
+++ app/src/Main.js
@@ -1,13 +1,58 @@
-const Main = (props) => {
+import {useState} from "react"
 
-    props.nombre = "Carlos"
+
+const Main = (props) => {
+    
+    
+    /* const resultado = useState() //Siempre retorna un array con dos elementos
+    const estado = resultado[0]
+    const setEstado = resultado[1] */
+
+    const [contador, setContador] = useState(props.initial)
+
+    /* let contador = 0
+    contador = "nuevoValor" */
+
+    //
+    /* let contador = 0
+    let darkMode = false
+    let logged = false
+    let carrito = []
+    let productos = []
+    let nombre = "" */
+    /* 
+    const btn = document.getElementById("btn")
+    btn.addEventListener("click", () => {
+        contador++
+        const p = document.getElementById("p")
+        p.innerHTML = `Hola Mundo ${contador}`
+    }) 
+    */
+
+    const handleClick = () => {
+       /*  contador++
+        console.log(contador) */
+        //contador = contador + 1
+        //contador++
+        //setContador(contador++)
+        setContador(contador + 1)
+    }
+
+    const restar = () => {
+        setContador(contador - 1)
+    }
+
+    const resetear = () => {
+        setContador(0)
+    }
 
     return (
         <main className="container">
             <h2>Bienvenido {props.nombre} {props.apellido}!</h2>
-            <p>Tengo {props.edad} años</p>
-            <p>{props.usuarios}</p>
-            {props.children}
+            <p>Mi contador actual : {contador}</p>
+            <button onClick={handleClick}>aumentar</button>
+            <button onClick={resetear}>resetear</button>
+            <button onClick={restar}>restar</button>
         </main>
     );
 }
