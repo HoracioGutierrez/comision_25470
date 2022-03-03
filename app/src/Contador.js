187933d33820af89a99b0cdfbf70e2f99d7df78d
Horacio Gutierrez <horacio.gutierrez@educacionit.com.ar>
2022-03-02 22:40:29 -0300

    Promesas

---
+++ app/src/Contador.js
@@ -0,0 +1,21 @@
+import { useState , useEffect } from "react"
+
+const Contador = () => {
+    
+    let [dark,setDark] = useState(false)
+    
+    const toggleDarkMode = () => {
+        setDark(!dark)
+    }
+    
+    return (
+        <div>
+            <h1>Contador</h1>
+            <p>El tema del sitio es : {dark.toString()}</p>
+            <p>Mi contador Actual va : 0</p>
+            <button onClick={toggleDarkMode}>toggle dark mode</button>
+        </div>
+    )
+}
+
+export default Contador 
--- app/src/Main.js
+++ app/src/Main.js
@@ -1,58 +1,78 @@
-import {useState} from "react"
+import { useState, useEffect } from "react"
+import Contador from "./Contador";
 
+
+let productosIniciales = [
+    {
+        id: 1,
+        nombre: "Producto 1",
+        precio: 100
+    },
+    {
+        id: 2,
+        nombre: "Producto 2",
+        precio: 200
+    },
+    {
+        id: 3,
+        nombre: "Producto 3",
+        precio: 300
+    }
+]
 
 const Main = (props) => {
-    
-    
-    /* const resultado = useState() //Siempre retorna un array con dos elementos
-    const estado = resultado[0]
-    const setEstado = resultado[1] */
 
-    const [contador, setContador] = useState(props.initial)
+    const [loading, setLoading] = useState(true)
+    const [productos, setProductos] = useState([])
 
-    /* let contador = 0
-    contador = "nuevoValor" */
+    //useEffect(function [,array])
 
-    //
-    /* let contador = 0
-    let darkMode = false
-    let logged = false
-    let carrito = []
-    let productos = []
-    let nombre = "" */
-    /* 
-    const btn = document.getElementById("btn")
-    btn.addEventListener("click", () => {
-        contador++
-        const p = document.getElementById("p")
-        p.innerHTML = `Hola Mundo ${contador}`
-    }) 
-    */
+    //Este efecto se va a ejecutar despues del montado el componente y despues de cada renderizado
+    useEffect(()=>{
 
-    const handleClick = () => {
-       /*  contador++
-        console.log(contador) */
-        //contador = contador + 1
-        //contador++
-        //setContador(contador++)
-        setContador(contador + 1)
-    }
+        //Aca estoy simulando que hago un pedido a una API
+        //Promise : Son objetos que prometen una funcionalidad que dura en el tiempo pero te "avisan" con su metodo .then() cuando termino todo bien o con su metodo .catch() cuando termino todo mal
+        //DRY : Don't Repeat Yourself
+        
+        const promesa = new Promise((res,rej)=>{
+            setTimeout(()=>{
+                res(productosIniciales)
+            },3000)
+        })
 
-    const restar = () => {
-        setContador(contador - 1)
-    }
+        //console.log(promesa)
 
-    const resetear = () => {
-        setContador(0)
-    }
+        promesa
+        .then((respuestaDeLaApi)=>{
+            setProductos(productosIniciales)
+        })
+        .catch((errorDeLaApi)=>{
+            console.log(errorDeLaApi)
+            //setError("Hubo un error al cargar los datos, vuelva a intentarlo por favor... o no")
+        })
+        .finally(()=>{
+            setLoading(false)
+        })
+        
+        
+        
+
+    })
+
+    console.log(productos)
 
     return (
         <main className="container">
             <h2>Bienvenido {props.nombre} {props.apellido}!</h2>
-            <p>Mi contador actual : {contador}</p>
-            <button onClick={handleClick}>aumentar</button>
-            <button onClick={resetear}>resetear</button>
-            <button onClick={restar}>restar</button>
+            <img src="/imagenes/logo192.png" />
+            <p>La cantidad de productos es : 0</p>
+            <p>{loading ? "Cargando..." : "Ya tenes los productos"}</p>
+            <ul>
+                {productos.map((producto,indice)=>{
+                    return <li>{producto.nombre}</li>
+                })}
+            </ul>
+            <Contador />
         </main>
     );
 }
